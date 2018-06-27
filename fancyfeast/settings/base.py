4ca2ebb4105e257159319c9ceb5780a70f23e480
Leon Lei <leonlei35t@gmail.com>
2018-06-26 22:32:59 -0400

    Enhancement: Moved staticfiles from fancyfeast to accounts

--- fancyfeast/settings/base.py
+++ fancyfeast/settings/base.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'fancyfeast/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'fancyfeast/media')
