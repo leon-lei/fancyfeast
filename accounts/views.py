e6a213c6fe3f82a46090dfbc0ba8a585f8758f70
Leon Lei <leonlei35t@gmail.com>
2018-07-06 00:14:14 -0400

    Fix: Registration flow from homepage

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,40 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import View
 
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect(reverse('accounts:view_profile'))
+        else:
+            return redirect(reverse('accounts:change_password'))
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:view_profile'))
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
+
+def view_profile(request, pk=None):
+    if pk:
+        user = User.objects.get(pk=pk)
+    else:
+        user = request.user
+    args = {'user': user}
+    return render(request, 'accounts/profile.html', args)
+
+############################### NOT CURRENTLY USING ###############################
 class RegistrationView(View):
     form_class = RegistrationForm
     template_name = 'accounts/reg_form.html'
@@ -33,55 +67,9 @@ class RegistrationView(View):
 
             # returns User object if credentials are correct
             user = authenticate(username=username, password=password)
-
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect(reverse('accounts:home'))
+                    return redirect(reverse('accounts:view_profile'))
 
         return render(request, self.template_name, {'form':form})
-
-def view_profile(request, pk=None):
-    if pk:
-        user = User.objects.get(pk=pk)
-    else:
-        user = request.user
-    args = {'user': user}
-    return render(request, 'accounts/profile.html', args)
-
-def edit_profile(request):
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('accounts:view_profile'))
-    else:
-        form = EditProfileForm(instance=request.user)
-        args = {'form': form}
-        return render(request, 'accounts/edit_profile.html', args)
-
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(user=request.user, data=request.POST)
-        if form.is_valid():
-            form.save()
-            update_session_auth_hash(request, form.user)
-            return redirect(reverse('accounts:view_profile'))
-        else:
-            return redirect(reverse('accounts:change_password'))
-    else:
-        form = PasswordChangeForm(user=request.user)
-        args = {'form': form}
-        return render(request, 'accounts/change_password.html', args)
-
-############################### NOT CURRENTLY USING ###############################
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('accounts:home'))
-    else:
-        form = RegistrationForm()
-        args = {'form':form}
-        return render(request, 'accounts/reg_form.html',args)
--- fancyfeast/views.py
+++ fancyfeast/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -11,9 +12,21 @@ def home(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(reverse('accounts:view_profile'))
+            user = form.save(commit=False)
+            # cleaned (normalized) data
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user.set_password(password)
+            user.save()
+
+            # returns User object if credentials are correct
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect(reverse('accounts:view_profile'))
+        return render(request, 'accounts/home.html', {'form':form})
     else:
         form = RegistrationForm()
-        args = {'form':form}
-        return render(request, 'accounts/home.html',args)
+        return render(request, 'accounts/home.html', {'form':form})
